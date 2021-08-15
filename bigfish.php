21669e31296015ae2d5afc783cd128817f1c508c
cooltech <mootrix@gmail.com>
2021-08-15 19:29:20 +0200

    Add files via upload

---
+++ bigfish.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+/* CSS styles */
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.frame {
+  display: block;
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  margin: 0;
+  padding: 0;
+  border: 0 none;
+  box-sizing: border-box;
+}
+
+</style>
+</head>
+<body>
+
+<iframe src="https://sacardace-a.akamaihd.net/aceFacebook/?v=12.3.14" class="frame"/>
+
+</body>
+</html> 
